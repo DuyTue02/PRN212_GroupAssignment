db51c3bafe2585760eee1ceb912ecfe03247bb51
DuyTue02 <105305497+DuyTue02@users.noreply.github.com>
2025-10-25 11:41:17 +0700

    Add files via upload

---
+++ BLL/Service/UserAccountService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DAL.Entities;
+using DAL.Repositories;
+
+namespace BLL.Services
+{
+    public class UserAccountService
+    {
+        private readonly UserAccountRepository _repository;
+
+        public UserAccountService()
+        {
+            _repository = new UserAccountRepository();
+        }
+
+        public User GetUserAccount(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            return _repository.GetUserAccount(email, password);
+        }
+    }
+}
---
+++ BLL/Service/VehicleService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DAL.Entities;
+using DAL.Repositories;
+using System.Collections.Generic;
+
+namespace BLL.Services
+{
+    public class VehicleService
+    {
+        private readonly VehicleRepository _repository;
+
+        public VehicleService()
+        {
+            _repository = new VehicleRepository();
+        }
+
+        public List<Vehicle> GetVehicles()
+        {
+            return _repository.GetAll();
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            _repository.Add(vehicle);
+        }
+
+        public void UpdateVehicle(Vehicle vehicle)
+        {
+            _repository.Update(vehicle);
+        }
+
+        public void DeleteVehicle(Vehicle vehicle)
+        {
+            _repository.Delete(vehicle);
+        }
+
+        public List<Vehicle> SearchVehicles(string keyword)
+        {
+            return _repository.Search(keyword);
+        }
+    }
+}
